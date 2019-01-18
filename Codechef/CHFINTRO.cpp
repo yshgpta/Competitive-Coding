ca15c8466a2e0d73b626172db77989debd6d349a
Yash <ysh.gpta8333@gmail.com>
2019-01-18 20:02:16 +0530

    Added new code

---
+++ Codechef/CHFINTRO.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n,r,a;
+  cin>>n>>r;
+  while(n--){
+    cin>>a;
+    if(a<r)
+    cout<<"Bad boi"<<endl;
+    else
+    cout<<"Good boi"<<endl;
+  }
+  return 0;
+}
