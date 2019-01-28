cf1eabbbf8235465930aa00991688c8349b94b1a
Yash <ysh.gpta8333@gmail.com>
2019-01-29 01:44:15 +0530

    Added new code

---
+++ Codechef/ADASCHOOL.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    int n,m;
+    cin>>n>>m;
+    if(n%2==0||m%2==0)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+  }
+  return 0;
+}
