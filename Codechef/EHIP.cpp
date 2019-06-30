a2e86cdad7b73c0e9675cac34db1796215940a03
Yash <ysh.gpta8333@gmail.com>
2019-06-30 13:19:27 +0530

    Added new code

---
+++ Codechef/EHIP.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ typedef long long ll;
+ #define vi vector<ll>
+ #define vvi vector<vi>
+ #define pii pair<ll,ll>
+ #define vpii vector<pair<ll,ll> >
+ #define mp make_pair
+ #define pb push_back
+ #define INF 1000000000000000
+ #define MOD 1000000007
+ #define ff first
+ #define ss second
+ #define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+
+ int main(){
+ 	FastIO
+ 	string str;
+  cin>>str;
+  set<char> st;
+  for(int i=0;i<str.length();i++)
+  st.insert(str[i]);
+  cout<<st.size();
+ 	return 0;
+ }
