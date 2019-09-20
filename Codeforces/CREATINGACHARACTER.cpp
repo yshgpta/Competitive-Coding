0f1f7ee7a4a094552270927871ad38afb8010469
Yash Gupta <ysh.gpta8333@gmail.com>
2019-09-21 03:11:16 +0530

    Added new code

---
+++ Codeforces/CREATINGACHARACTER.cpp
@@ -0,0 +1,35 @@
+//ysh_gpta
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define vb vector<bool>
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pii pair<ll,ll>
+#define vpii vector<pair<ll,ll> >
+#define mkp make_pair
+#define pb push_back
+#define INF 1000000000000000
+#define MOD 1000000007
+#define ff first
+#define ss second
+#define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//ll gcd( ll a, ll b ){if(b==0) return a; else return gcd( b, a%b );}
+//ll ncr(ll n,ll r){ll ans=1;r=min(r,n-r);for (int i=1;i<=r;i++){ans*=(n-r+i);ans/=i;}return ans;}
+int main(){
+   FastIO
+   int t;
+   cin>>t;
+   while(t--){
+       ll a,b,c;
+       cin>>a>>b>>c;
+       if(a+c <= b){
+           cout<<0<<endl;
+           continue;
+       }
+       double val = a+c-b;
+       ll res = ceil(val/2);
+       cout<<res<<endl;
+   }
+   return 0;
+}
