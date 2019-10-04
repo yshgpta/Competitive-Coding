3753ddc5aab28b08f1cab566aea4404d65c6889a
Yash Gupta <ysh.gpta8333@gmail.com>
2019-10-05 01:07:56 +0530

    Added new code

--- Codechef/INOI1401.cpp
+++ Codechef/INOI1401.cpp
@@ -1,47 +1,53 @@
-#include <iostream>
-#include <ctime>
-
+//ysh_gpta
+#include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+#define vb vector<bool>
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pii pair<ll,ll>
+#define vpii vector<pair<ll,ll> >
+#define mkp make_pair
+#define pb push_back
+#define INF 1000000000000000
+#define MOD 1000000007
+#define ff first
+#define ss second
+#define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//ll gcd( ll a, ll b ){if(b==0) return a; else return gcd( b, a%b );}
+//ll ncr(ll n,ll r){ll ans=1;r=min(r,n-r);for (int i=1;i<=r;i++){ans*=(n-r+i);ans/=i;}return ans;}
 
-int R, C, D;
+bool grid[310][310];
+ll dp[310][310][310][2];
+ll r,c,d;
 
-bool av[305][305];
-int ans[305][305][305][2] = { };
-
-int main()
-{
-        cin >> R >> C >> D;
-
-        for(int i = 1; i <= R; ++i)
-        {
-                for(int j = 1; j <= C; ++j)
-                {
-                        cin >> av[i][j];
-                }
+ll solve(ll i,ll j,ll can,bool dn){
+        if(i==r-1 && j==c-1)
+        return 1;
+        if(i>=r || i>=c || grid[i][j]==0)
+        return 0;
+        if(dp[i][j][can][dn]!=-1)
+        return dp[i][j][can][dn];
+        ll x=0;
+        if(dn==1){
+                if(can<d)
+                x = (x + solve(i+1,j,can+1,1))%20011;
+                x = (x + solve(i,j+1,1,0))%20011;
+        }else{
+                x = (x + solve(i+1,j,1,1))%20011;
+                if(can<d)
+                x = (x + solve(i,j+1,can+1,0))%20011; 
         }
+        return dp[i][j][can][dn]=x;
+}
 
-        for(int d = 0; d <= D; d++)
-        {
-                ans[R][C][d][0] = ans[R][C][d][1] = 1;
-        }
-
-        for(int r = R; r > 0; r--)
-        {
-                for(int c = C; c > 0; c--)
-                {
-                        if(r == R && c == C) continue;
-                        if(av[r][c])
-                        {
-                                ans[r][c][D][0] = ans[r + 1][c][1][1];
-                                ans[r][c][D][1] = ans[r][c + 1][1][0];
-                                for(int d = 0; d < D; d++)
-                                {
-                                        ans[r][c][d][0] = (ans[r][c + 1][d + 1][0] + ans[r + 1][c][1][1]) % 20011;
-                                        ans[r][c][d][1] = (ans[r][c + 1][1][0] + ans[r + 1][c][d + 1][1]) % 20011;
-                                }
-                        }
-                }
-        }
-
-        cout << ans[1][1][0][0];
+int main(){
+   FastIO
+   cin>>r>>c>>d;
+   for(int i=0;i<r;i++)
+   for(int j=0;j<c;j++)
+   cin>>grid[i][j];
+   memset(dp,-1,sizeof(dp));
+   cout<<solve(0,0,0,0);
+   return 0;
 }
