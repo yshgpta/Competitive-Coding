6528ad20e6bd9d12feb5278ef692c7594865fa75
Yash Gupta <ysh.gpta8333@gmail.com>
2019-10-16 22:36:20 +0530

    Added new code

--- test.cpp
+++ test.cpp
@@ -1,131 +1,46 @@
-#include <iostream>
-#include <string.h>
-#include <stdio.h>
-#include <stack>
+//ysh_gpta
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
 using namespace std;
-#define SIZE 20001
-int  parent[SIZE] = {0};
-int  size[SIZE] = { 0 };
-typedef struct {
-	int a;
-	int b;
-	bool mark;
-}EDG;
-EDG edg[SIZE] = { 0 };
-
-int ROOT(int x)
-{
-	while (x != parent[x])
-	{
-		parent[x] = parent[parent[x]];
-		x = parent[x];
+typedef long long ll;
+#define vb vector<bool>
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pii pair<ll,ll>
+#define vpii vector<pair<ll,ll> >
+#define mkp make_pair
+#define pb push_back
+#define INF 1000000000000000
+#define MOD 1000000007
+#define ff first
+#define ss second
+#define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//ll gcd( ll a, ll b ){if(b==0) return a; else return gcd( b, a%b );}
+//ll ncr(ll n,ll r){ll ans=1;r=min(r,n-r);for (int i=1;i<=r;i++){ans*=(n-r+i);ans/=i;}return ans;}
+ll solve(ll len,ll sum,vi &v){
+	if(len<0)
+	return INT_MIN;
+	if(len==0)
+	return sum;
+	ll mx = INT_MIN;
+	for(int i=1;i<=v.size();i++){
+		mx = max(mx,solve(len-i,sum+v[i-1],v));
 	}
-	return x;
+	return mx;
 }
-void UNION(int rx, int ry)
-{
-	if (size[rx] > size[ry])
-	{
-		parent[ry] = rx;
-		size[rx] = size[rx] + size[ry];
-	}
-	else
-	{
-		parent[rx] = ry;
-		size[ry] = size[ry] + size[rx];
-	}
-}
-int main()
-{
-///	freopen("NITTROAD_input.txt", "r", stdin);
-	int T;
-	scanf("%d",&T);
-	while (T--)
-	{
-		int Nodes, edgeCount, a, b;
-		scanf("%d", &Nodes);;
-		edgeCount = Nodes-1;
-		for (int i = 1; i <= Nodes; i++)
-		{
-			parent[i] = i;
-			size[i] = 1;
-		}
-		for (int i = 1; i <= edgeCount; i++)
-		{
-			// cin >> edg[i].a >> edg[i].b;
-			scanf("%d %d", &edg[i].a, &edg[i].b);	
-			edg[i].mark = false;
-		}
-		int Q = 0;
-		scanf("%d", &Q);
-		getchar();
-		char ch;
-		stack<int> R;
-		
-		while (Q--)
-		{
-			scanf("%c", &ch);
-			if (ch == 'R')
-			{
-				int k;
-				scanf("%d", &k);
-				R.push(k); // push in stack
-				edg[k].mark = true;
-			}
-			else if (ch == 'Q')
-			{
-				R.push(0); //push 0 to mark Query
-			}
-			getchar();
-		}
-		unsigned long long  ConnPair = 0;
-		// Preapare set of all nodes except the one which are MARKED
-		for (int i = 1; i <= edgeCount; i++)
-		{
-			if (edg[i].mark == false)
-			{
-				int ra = ROOT(edg[i].a);
-				int rb = ROOT(edg[i].b);
-				if (ra != rb)
-				{
-					ConnPair = ConnPair + (size[ra] * size[rb]); // Keep counting the connected Pairs
-					UNION(ra, rb);
-				}
-			}
-		}
 
-		unsigned long long  DicConnPair = (Nodes * (Nodes - 1) / 2) - ConnPair;
-		stack<unsigned long long> result;
-		/// extract Remove operation from STACK and save the ANswer in another stack 
-		while (!R.empty())
-		{
-			int popped = R.top();
-			R.pop();
-			if (popped > 0)
-			{
-				int ra = ROOT(edg[popped].a);
-				int rb = ROOT(edg[popped].b);
-				if (ra != rb)
-				{
-					DicConnPair = DicConnPair - (size[ra] * size[rb]); // Keep decrementing the pairs based on UNION 
-					UNION(ra, rb);
-				}
-			}
-			else
-			{
-				result.push(DicConnPair);
-			}
-
-		}
-		//ans
-		while (!result.empty()) {
-
-			printf("%llu\n", result.top());
-			result.pop();
-		}
-		if (T > 0)
-			printf("\n");
-		
-	}
-	return 0;
+int main(){
+   FastIO
+   ll n;
+   cin>>n;
+   vi v(n);
+   for(int i=0;i<n;i++)
+   cin>>v[i];
+   ll len;
+   cin>>len;
+   cout<<solve(len,0,v);
+   return 0;
 }
