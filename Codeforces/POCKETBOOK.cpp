fe790965b85d08585f7804a003b488601a16d2bd
Yash Gupta <ysh.gpta8333@gmail.com>
2019-11-05 18:08:58 +0530

    Added new code

---
+++ Codeforces/POCKETBOOK.cpp
@@ -0,0 +1,44 @@
+//ysh_gpta
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+using namespace std;
+typedef long long ll;
+#define vb vector<bool>
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pii pair<ll,ll>
+#define vpii vector<pair<ll,ll> >
+#define mkp make_pair
+#define pb push_back
+#define INF 1000000000000000
+#define MOD 1000000007
+#define ff first
+#define ss second
+#define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//ll gcd( ll a, ll b ){if(b==0) return a; else return gcd( b, a%b );}
+//ll ncr(ll n,ll r){ll ans=1;r=min(r,n-r);for (int i=1;i<=r;i++){ans*=(n-r+i);ans/=i;}return ans;}
+int main(){
+   FastIO
+   ll n,m;
+   cin>>n>>m;
+   vector<string> v(n);
+   for(int i=0;i<n;i++)
+   cin>>v[i];
+   ll ans=1;
+   for(int k=0;k<m;k++){
+       map<char,int> mp;
+       for(int i=0;i<n;i++){
+           mp[v[i][k]]++;
+       }
+       ll count=0;
+       for(auto it : mp){
+           count++;
+       }
+       ans =(ans * count)%MOD;
+   }
+   cout<<ans;
+   return 0;
+}
