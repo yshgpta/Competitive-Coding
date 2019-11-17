8ecba8ee132086be76b10153404e7999eb3c4b03
Yash Gupta <ysh.gpta8333@gmail.com>
2019-11-17 23:56:38 +0530

    Added new code

---
+++ Codechef/CHEFDIET.cpp
@@ -0,0 +1,53 @@
+//ysh_gpta
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+using namespace std;
+typedef long long ll;
+#define vb vector<bool>
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pii pair<ll,ll>
+#define vpii vector<pair<ll,ll> >
+#define mkp make_pair
+#define pb push_back
+#define INF 1000000000000000
+#define MOD 1000000007
+#define ff first
+#define ss second
+#define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//ll gcd( ll a, ll b ){if(b==0) return a; else return gcd( b, a%b );}
+//ll ncr(ll n,ll r){ll ans=1;r=min(r,n-r);for (int i=1;i<=r;i++){ans*=(n-r+i);ans/=i;}return ans;}
+//void printArray(vi &v){for(int i=0;i<v.size();i++) cout<<v[i]<<endl; cout<<endl; }
+int main(){
+   FastIO
+   ll t;
+   cin>>t;
+   while(t--){
+       ll n,m;
+       cin>>n>>m;
+       vi v(n);
+       for(int i=0;i<n;i++){
+           cin>>v[i];
+       }
+       ll excess =0 ;
+       ll flag=0;
+       ll res=0;
+       for(int i=0;i<n;i++){
+           if(v[i]+excess>=m){
+               excess = v[i] + excess - m;
+           }else{
+               flag=1;
+               res = i+1;
+               break;
+           }
+       }
+       if(flag==1)
+       cout<<"NO"<<" "<<res<<endl;
+       else
+       cout<<"YES"<<endl;
+   }
+   return 0;
+}
