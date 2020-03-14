126e7238f52d026aebc8178072c92d3815f5690b
Yash Gupta <ysh.gpta8333@gmail.com>
2020-03-14 23:26:48 +0530

    Added new code

--- test.cpp
+++ test.cpp
@@ -20,27 +20,22 @@ typedef long long ll;
 #define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 //ll gcd( ll a, ll b ){if(b==0) return a; else return gcd( b, a%b );}
 //ll ncr(ll n,ll r){ll ans=1;r=min(r,n-r);for (int i=1;i<=r;i++){ans*=(n-r+i);ans/=i;}return ans;}
-ll solve(ll len,ll sum,vi &v){
-	if(len<0)
-	return INT_MIN;
-	if(len==0)
-	return sum;
-	ll mx = INT_MIN;
-	for(int i=1;i<=v.size();i++){
-		mx = max(mx,solve(len-i,sum+v[i-1],v));
-	}
-	return mx;
-}
-
+//void printArray(vi &v){for(int i=0;i<v.size();i++) cout<<v[i]<<endl; cout<<endl; }
 int main(){
    FastIO
    ll n;
    cin>>n;
-   vi v(n);
-   for(int i=0;i<n;i++)
-   cin>>v[i];
-   ll len;
-   cin>>len;
-   cout<<solve(len,0,v);
+   ll dp1[n],dp2[n],dp3[n],dp4[n];
+   dp1[0] = 2;
+   dp2[0] = 3;
+   dp3[0] = 4;
+   dp4[0] = 1;
+   for(int i=1;i<n;i++){
+	   dp1[i] = 2*dp2[i-1]+dp1[i-1];
+	   dp2[i] = 2*dp1[i-1]+dp3[i-1]+dp2[i-1];
+	   dp3[i] = 3*dp1[i-1]+dp3[i-1]+dp3[i-1];
+	   dp4[i] = dp3[i-1]+dp4[i-1];
+   }
+   cout<<dp1[n-1] + dp2[n-1] + dp3[n-1] + dp4[n-1];
    return 0;
 }
