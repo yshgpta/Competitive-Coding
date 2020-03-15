13714ec2716c418fb660fd73d9a0a69285ce0853
Yash Gupta <ysh.gpta8333@gmail.com>
2020-03-15 22:17:43 +0530

    Added new code

---
+++ Codechef/ALR20E.cpp
@@ -0,0 +1,52 @@
+//ysh_gpta
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+using namespace std;
+typedef long long ll;
+#define vb vector<bool>
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pii pair<ll,ll>
+#define vpii vector<pair<ll,ll> >
+#define mkp make_pair
+#define pb push_back
+#define INF 1000000000000000
+#define MOD 1000000007
+#define ff first
+#define ss second
+#define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//ll gcd( ll a, ll b ){if(b==0) return a; else return gcd( b, a%b );}
+//ll ncr(ll n,ll r){ll ans=1;r=min(r,n-r);for (int i=1;i<=r;i++){ans*=(n-r+i);ans/=i;}return ans;}
+//void printArray(vi &v){for(int i=0;i<v.size();i++) cout<<v[i]<<endl; cout<<endl; }
+int main(){
+   FastIO
+   ll t;
+   cin>>t;
+   while(t--){
+       ll n;
+       cin>>n;
+       ll a=0;
+       vi v(n);
+       for(int i=1;i<n;i++){
+           cin>>v[i];
+           a ^= i;
+       }
+       a^=n;
+       ll b=0;
+       for(int i=1;i<n;i+=2){
+           b ^= v[i];
+       }
+       vi res(n+1);
+       res[n]=b^a;
+       for(int i=n-1;i>=1;i--){
+           res[i] = res[i+1]^v[i];
+       }
+       for(int i=1;i<=n;i++)
+       cout<<res[i]<<" ";
+       cout<<endl;
+   }
+   return 0;
+}
