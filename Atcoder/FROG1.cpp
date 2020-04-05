2975839c06a1f2862c6bbf32d361d4bef18497a0
Yash Gupta <ysh.gpta8333@gmail.com>
2020-04-05 14:46:44 +0530

    Added new code

---
+++ Atcoder/FROG1.cpp
@@ -0,0 +1,40 @@
+//ysh_gpta
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+using namespace std;
+typedef long long ll;
+#define vb vector<bool>
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pii pair<ll,ll>
+#define vpii vector<pair<ll,ll> >
+#define mkp make_pair
+#define pb push_back
+#define INF 1000000000000000
+#define MOD 1000000007
+#define ff first
+#define ss second
+#define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//ll gcd( ll a, ll b ){if(b==0) return a; else return gcd( b, a%b );}
+//ll ncr(ll n,ll r){ll ans=1;r=min(r,n-r);for (int i=1;i<=r;i++){ans*=(n-r+i);ans/=i;}return ans;}
+void printArray(vi &v){for(int i=0;i<v.size();i++) cout<<v[i]<<" "; cout<<endl; }
+int main(){
+   FastIO
+   ll n;
+   cin>>n;
+   vi v(n);
+   for(int i=0;i<n;i++)
+   cin>>v[i];
+   vi dp(n);
+   dp[0]=0;
+   dp[1] = abs(v[1]-v[0]);
+   for(int i=2;i<n;i++){
+       dp[i] = min(abs(v[i]-v[i-1])+dp[i-1],abs(v[i]-v[i-2])+dp[i-2]);
+   }
+//    printArray(dp);
+   cout<<dp[n-1];
+   return 0;
+}
