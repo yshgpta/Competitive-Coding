5eb9341dfab1f2b1e9c49c658638ca1f4a1b7975
Yash Gupta <ysh.gpta8333@gmail.com>
2020-04-10 17:26:36 +0530

    Added new code

---
+++ Atcoder/INDEPENDENTSET.cpp
@@ -0,0 +1,90 @@
+//ysh_gpta
+#include<bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+//using multiInt = boost::multiprecision::cpp_int;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+using namespace std;
+
+
+#define bit(x,i) (x&(1<<i))  //select the bit of position i of x
+#define IN(i,l,r) (l<i&&i<r)   //the next for are for checking bound
+#define LINR(i,l,r) (l<=i&&i<=r)
+#define LIN(i,l,r) (l<=i&&i<r)
+#define INR(i,l,r) (l<i&&i<=r)
+
+
+#define repf(i,L,R) for (int i = L; i < R; i++) //next four are for "for loops"
+#define repfe(i,L,R) for (int i = L; i <= R; i++)
+#define repb(i,L,R) for (int i = L; i > R; i--)
+#define repbe(i,L,R) for (int i = L; i >= R; i--)
+
+
+typedef long long ll;
+#define cerr if(false)cerr
+#define vb vector<bool>
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pii pair<ll,ll>
+#define vpii vector<pair<ll,ll> >
+#define mkp make_pair
+#define pb push_back
+#define INF 1000000000000000
+#define MOD 1000000007
+#define ff first
+#define ss second
+
+
+#define all(v) v.begin(), v.end()
+#define NP(v) next_permutation(all(v))
+#define watch(x) cerr << (#x) << " is " << (x) << endl;
+#define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+ll gcd( ll a, ll b ){if(b==0) return a; else return gcd( b, a%b );}
+ll ncr(ll n,ll r){ll ans=1;r=min(r,n-r);for (int i=1;i<=r;i++){ans*=(n-r+i);ans/=i;}return ans;}
+
+
+vector<int> Dx = {0, 0, -1, 1, -1, 1, -1, 1, 0};
+vector<int> Dy = {1, -1, 0, 0, -1, -1, 1, 1, 0};
+
+ll dp[100005][2];
+
+vvi adj(100005);
+
+void dfs(ll node,ll from){
+    dp[node][0]=1;
+    dp[node][1]=1;
+    if(from!=-1 && adj[node].size()==1)
+    return;
+    repf(i,0,adj[node].size()){
+        ll to = adj[node][i];
+        if(to != from){
+            dfs(to,node);
+            dp[node][0] *= (dp[to][0]+dp[to][1])%MOD;
+            dp[node][0] %= MOD;
+            dp[node][1] *= dp[to][0];
+            dp[node][1] %= MOD;
+        }
+    }
+}
+
+int main(){
+    FastIO
+    ll n;
+    cin>>n;
+    repf(i,0,n-1){
+        ll a,b;
+        cin>>a>>b;
+        a--,b--;
+        adj[a].pb(b);
+        adj[b].pb(a);
+    }
+    memset(dp,-1,sizeof(dp));
+    dfs(0,-1);
+    cout<<(dp[0][0]+dp[0][1])%MOD;
+
+    return 0;
+}
